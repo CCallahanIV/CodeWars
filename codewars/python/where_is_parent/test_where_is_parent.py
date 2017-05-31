46da5526e0cc7cd622e96b97176263d9fb7194c5
Ted Callahan <CCallahanIV@gmail.com>
2017-05-30 21:16:11 -0700

    Moved files around, started play_with_digits.

---
+++ codewars/python/where_is_parent/test_where_is_parent.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+TEST_PARAMS = [
+    ("abBA", "AaBb"),
+    ("AaaaaZazzz", "AaaaaaZzzz"),
+    ("CbcBcbaA", "AaBbbCcc"),
+    ("xXfuUuuF", "FfUuuuXx"),
+    ("", ""),
+    ("ABbb", "ABbb"),
+    ('soZqoUrlkvnzFkflEinAwlWKzrqxixszisLewammvnoQwmnSlRgoriYxNGmvgwzMJoxVlzXzfiOmzlfiHixmyIxwgyi', 'AaEeFfffGgggHIiiiiiiiiJKkkLllllllMmmmmmmNnnnnOoooooQqqRrrrSsssUVvvvWwwwwwXxxxxxxYyyZzzzzzzz')
+]
+
+
+@pytest.mark.parametrize("qry, res", TEST_PARAMS)
+def test_where_is_parent(qry, res):
+    """Test the card sort kata."""
+    from where_is_parent import find_children
+    assert find_children(qry) == res
